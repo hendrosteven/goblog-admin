ee430e09393dcf35e2685235c245d21de8f5c435
Hendro Steven <hendro.steven@gmail.com>
2018-01-27 14:40:58 +0800

    edit category

--- apps/controllers/CategoryController.php
+++ apps/controllers/CategoryController.php
@@ -51,10 +51,55 @@ class CategoryController extends Controller {
     }
 
     function edit(){
-        
+        $id = $this->f3->get('PARAMS.id');
+        $v = new Valitron\Validator(array('Category ID' => $id));
+        $v->rule('required', ['Category ID']);
+        if ($v->validate()) {           
+            $category = new Category($this->db);
+            $this->f3->set('category', $category->load(array('id=?', $id)));
+            $this->f3->set('view', 'pages/category/edit.html');
+        } else {
+            $this->f3->reroute('/categories');
+        }
     }
 
     function update(){
-        
+        $id = $this->f3->get('POST.id');
+        $categoryName = $this->f3->get('POST.categoryName');             
+        $categoryStatus = $this->f3->get('POST.categoryStatus');
+
+        $v = new Valitron\Validator(array('Category Name' => $categoryName));
+        $v->rule('required', ['Category Name']);   
+
+        if ($v->validate()) {
+            $category = new Category($this->db);
+            $category->load(array('id=?',$id));
+            $category->name = $categoryName;
+            $category->status = $categoryStatus;
+            try{
+                $category->update();
+                $flash = array(
+                    'errorType' => 'Success',
+                    'infos' => array(array('Data tersimpan'))
+                );
+                $this->f3->set('SESSION.flash', $flash);
+                $this->f3->reroute('/categories');
+            }catch(Exception $ex){
+                $flash = array(
+                    'errorType' => 'Error(s)',
+                    'errors' => array(array('Terjadi kesalahan, silahkan periksa data yang diinput')),
+                    'categoryName' => $categoryName
+                );
+                $this->f3->set('SESSION.flash', $flash);
+                $this->f3->reroute("/categories/edit/$id");
+            }
+        }else{
+            $flash = array(
+                'errorType' => 'Error(s)',
+                'errors' => $v->errors()
+            );
+            $this->f3->set('SESSION.flash', $flash);
+            $this->f3->reroute("/categories/edit/$id");
+        }
     }
 }
